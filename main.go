382c1e8a5540709e53d5b5d57594be7c8d069843
Georgii Staroselskii <gosha371@gmail.com>
2017-03-06 03:22:31 +0300

    Add personify package

--- main.go
+++ main.go
@@ -2,16 +2,13 @@ package main
 
 import (
 	"flag"
-	"image"
-	"image/draw"
 	"image/jpeg"
 	_ "image/png"
 	"os"
 	"path/filepath"
 
+	"github.com/staroselskii/afxize/personify"
 	"github.com/staroselskii/afxize/facefinder"
-
-	"github.com/disintegration/imaging"
 )
 
 var haarCascade = flag.String("haar", "haarcascade_frontalface_alt.xml", "The location of the Haar Cascade XML configuration to be provided to OpenCV.")
@@ -20,7 +17,7 @@ var facesDir = flag.String("faces", "faces", "The directory to search for faces.
 func main() {
 	flag.Parse()
 
-	var chrisFaces facefinder.FaceList
+	var personFaces facefinder.FaceList
 
 	var facesPath string
 	var err error
@@ -32,60 +29,17 @@ func main() {
 		}
 	}
 
-	err = chrisFaces.Load(facesPath)
+	err = personFaces.Load(facesPath)
 	if err != nil {
 		panic(err)
 	}
-	if len(chrisFaces) == 0 {
+	if len(personFaces) == 0 {
 		panic("no faces found")
 	}
 
 	file := flag.Arg(0)
 
-	finder := facefinder.NewFinder(*haarCascade)
-
-	baseImage := facefinder.LoadImage(file)
-
-	faces := finder.Detect(baseImage)
-
-	bounds := baseImage.Bounds()
-
-	canvas := facefinder.CanvasFromImage(baseImage)
-
-	for _, face := range faces {
-        rect := facefinder.RectMargin(30.0, face)
-
-		newFace := chrisFaces.Random()
-		if newFace == nil {
-			panic("nil face")
-		}
-		chrisFace := imaging.Fit(newFace, rect.Dx(), rect.Dy(), imaging.Lanczos)
-
-		draw.Draw(
-			canvas,
-			rect,
-			chrisFace,
-			bounds.Min,
-			draw.Over,
-		)
-	}
-
-	if len(faces) == 0 {
-		face := imaging.Resize(
-			chrisFaces[0],
-			bounds.Dx()/3,
-			0,
-			imaging.Lanczos,
-		)
-		face_bounds := face.Bounds()
-		draw.Draw(
-			canvas,
-			bounds,
-			face,
-			bounds.Min.Add(image.Pt(-bounds.Max.X/2+face_bounds.Max.X/2, -bounds.Max.Y+int(float64(face_bounds.Max.Y)/1.9))),
-			draw.Over,
-		)
-	}
+    var canvas = personify.Personify(file, haarCascade, personFaces)
 
 	jpeg.Encode(os.Stdout, canvas, &jpeg.Options{jpeg.DefaultQuality})
 }
---
+++ personify/afxize.go
@@ -0,0 +1,58 @@
+package personify
+
+import (
+	"image"
+	"image/draw"
+	"github.com/staroselskii/afxize/facefinder"
+
+	"github.com/disintegration/imaging"
+)
+
+func Personify(file string, haarCascade *string, personFaces facefinder.FaceList) image.Image {
+	finder := facefinder.NewFinder(*haarCascade)
+
+	baseImage := facefinder.LoadImage(file)
+
+	faces := finder.Detect(baseImage)
+
+	bounds := baseImage.Bounds()
+
+	canvas := facefinder.CanvasFromImage(baseImage)
+
+	for _, face := range faces {
+        rect := facefinder.RectMargin(30.0, face)
+
+		newFace := personFaces.Random()
+		if newFace == nil {
+			panic("nil face")
+		}
+		chrisFace := imaging.Fit(newFace, rect.Dx(), rect.Dy(), imaging.Lanczos)
+
+		draw.Draw(
+			canvas,
+			rect,
+			chrisFace,
+			bounds.Min,
+			draw.Over,
+		)
+	}
+
+	if len(faces) == 0 {
+		face := imaging.Resize(
+			personFaces[0],
+			bounds.Dx()/3,
+			0,
+			imaging.Lanczos,
+		)
+		face_bounds := face.Bounds()
+		draw.Draw(
+			canvas,
+			bounds,
+			face,
+			bounds.Min.Add(image.Pt(-bounds.Max.X/2+face_bounds.Max.X/2, -bounds.Max.Y+int(float64(face_bounds.Max.Y)/1.9))),
+			draw.Over,
+		)
+	}
+
+    return canvas
+}
